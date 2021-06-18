68ff3260c74cc7035e557e3b4ac563f6e9e068b3
Miguel Bomfanti <miguelbomfanti@hotmail.com>
2021-06-18 12:08:08 -0300

    teste

---
+++ src/DefaultValues.java
@@ -0,0 +1,20 @@
+public class DefaultValues {
+
+    public static void main(String[] args){
+
+        final Default d = new Default ();
+
+        System.out.println(d.getI());
+        System.out.println(d.isActive());
+
+    }
+
+    static class Default {
+        int i;
+        boolean active;
+
+        public int getI() { return i; }
+
+        public boolean isActive () { return active; }
+    }
+}
